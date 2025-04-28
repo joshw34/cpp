f1eea63440f64c36d9760aeb16f947f83564c7fb
joshw34 <joshwhitley@msn.com>
2025-04-28 11:01:55 +0200

    fixed formatting, moved m00e01 to correct dir

--- module00/ex00/src/megaphone.cpp
+++ module00/ex00/src/megaphone.cpp
@@ -2,13 +2,12 @@
 #include <cctype>
 #include <string>
 
-int	main(int ac, char **av)
-{
-	if (ac > 1)
-	{
-		for (int i = 1; i < ac; i++)
-		{
-			std::string arg = av[i];
+int	main(int ac, char **av) {
+	std::string	arg;
+
+	if (ac > 1) {
+		for (int i = 1; i < ac; i++) {
+			arg = av[i];
 			for (size_t j = 0; j != arg.length(); j++)
 				std::cout << (char)std::toupper(arg[j]);
 			if (i < ac - 1)
