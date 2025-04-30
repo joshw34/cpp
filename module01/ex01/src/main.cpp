a3db794282a42cb12d1ca6b191bef788d380fa64
joshw34 <joshwhitley@msn.com>
2025-04-30 14:05:10 +0200

    finsihed m01e01

---
+++ module01/ex01/src/main.cpp
@@ -0,0 +1,25 @@
+#include "../inc/Zombie.hpp"
+
+Zombie* zombieHorde(int n, std::string name);
+
+static	void	input(int &n, std::string &name) {
+	std::cout << "How many zombies?: ";
+	std::cin >> n;
+	std::cout << std::endl;
+
+	std::cout << "Zombie Name?: ";
+	std::cin >> name;
+	std::cout << std::endl;
+}
+
+int	main(void) {
+	int	n;
+	std::string name;
+
+	input(n, name);
+	Zombie* horde = zombieHorde(n, name);
+	for (int i = 0; i < n; ++i) {
+		horde[i].announce();
+	}
+	delete[] horde;
+}
