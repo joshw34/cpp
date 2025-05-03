62f463f81055958f593c38adfbd1d2291db74279
joshw34 <joshwhitley@msn.com>
2025-05-04 01:02:36 +0200

    m01e05 finished

---
+++ module01/ex05/src/main.cpp
@@ -0,0 +1,17 @@
+#include "../inc/Harl.hpp"
+
+int	main(void) {
+	Harl	h;
+	std::string	level;
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	
+	for (int i = 0; i < 4; i++) {
+		h.complain(levels[i]);
+		std::cout << "\n";
+	}
+	while (level.compare("EXIT") != 0) {
+		std::cout << "\nEnter DEBUG, INFO, WARNING, ERROR or EXIT\n> ";
+		std::cin >> level;
+		h.complain(level);
+	}
+}
