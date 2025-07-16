eaf1a50d5fd62588f79811ca7cba02d60110d0eb
joshw34 <joshwhitley@msn.com>
2025-07-16 10:03:41 +0200

    m00e00
    moved uppercase printing to a separate function

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
-//#include <cctype>
+#include <cctype>
 #include <string>
 
-int	main(int ac, char **av) {
+void printUppercase(int ac, char **av) {
 	std::string	arg;
 
-	if (ac > 1) {
-		for (int i = 1; i < ac; i++) {
-			arg = av[i];
-			for (size_t j = 0; j != arg.length(); j++)
-				std::cout << (char)std::toupper(arg[j]);
-			if (i < ac - 1)
-				std::cout << " ";
-		}
-	}
+    for (int i = 1; i < ac; i++) {
+	    arg = av[i];
+
+        for (size_t j = 0; j < arg.length(); j++)
+            std::cout << (char)std::toupper(arg[j]);
+        
+        if (i < ac - 1)
+            std::cout << " ";
+    }
+}
+
+int	main(int ac, char **av) {
+	if (ac > 1) 
+        printUppercase(ac, av);
 	else
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	std::cout << std::endl;
