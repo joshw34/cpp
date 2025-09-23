5fa8c4156e0ceff77ab6b7f09d38f7a06c2f70ff
joshw34 <joshwhitley@msn.com>
2025-09-23 09:55:32 +0200

    m09e02 continued

--- module09/ex02/inc/PmergeMe.hpp
+++ module09/ex02/inc/PmergeMe.hpp
@@ -4,6 +4,8 @@
 #include <cstdlib>
 #include <stdexcept> // IWYU pragma: keep
 #include <iostream>
+#include <list>
+#include <iterator>
 
 class PmergeMe {
  public:
@@ -17,9 +19,20 @@ class PmergeMe {
   PmergeMe(const PmergeMe&);
   PmergeMe& operator=(const PmergeMe&);
 
-  std::vector<int> data;
+  std::vector<int> vec_data;
+  std::list<int> lst_data;
 
   static const std::vector<int>& validateInput(const std::vector<int>&);
+  static const std::list<int> initDataList(const std::vector<int>&);
 
   void printData(const std::string);
+
+  template<typename Iterator>
+  void mergeInsertSort(Iterator start, Iterator end, size_t pair_level) {
+    (void)pair_level;
+    for (Iterator it = start; it != end; ++it) {
+      if (*it > *(std::next(it)))
+        std::swap(*it, *std::next(it));
+    }
+  }
 };
--- module09/ex02/src/PmergeMe.cpp
+++ module09/ex02/src/PmergeMe.cpp
@@ -2,35 +2,57 @@
 
 PmergeMe::PmergeMe() {}
 
-PmergeMe::PmergeMe(const std::vector<int>& input) : data(validateInput(input)) {}
+PmergeMe::PmergeMe(const std::vector<int>& input)
+    : vec_data(validateInput(input)), lst_data(initDataList(input)) {}
 
-PmergeMe::PmergeMe(const PmergeMe& src) : data(src.data) {}
+PmergeMe::PmergeMe(const PmergeMe& src)
+    : vec_data(src.vec_data), lst_data(src.lst_data) {}
 
 PmergeMe::~PmergeMe() {}
 
 PmergeMe& PmergeMe::operator=(const PmergeMe& src) {
   if (this != &src) {
-    this->data = src.data;
+    this->vec_data = src.vec_data;
+    this->lst_data = src.lst_data;
   }
   return *this;
 }
 
 void PmergeMe::sort() {
   printData("Before");
+  for (std::list<int>::iterator it = lst_data.begin(); it != lst_data.end();
+       ++it) {
+    std::cout << *it << " ";
+  }
+  std::cout << "\n";
+  mergeInsertSort(vec_data.begin(), vec_data.end(), 0);
+  mergeInsertSort(lst_data.begin(), lst_data.end(), 0);
+  printData("After");
+  for (std::list<int>::iterator it = lst_data.begin(); it != lst_data.end();
+       ++it) {
+    std::cout << *it << " ";
+  }
+  std::cout << "\n";
 }
 
 const std::vector<int>& PmergeMe::validateInput(const std::vector<int>& input) {
   for (size_t i = 0; i < input.size(); ++i) {
     if (input.at(i) < 1)
-      throw std::invalid_argument("Negative Value in Input");
+      throw std::invalid_argument("Non-positive Value in Input");
   }
-  return input; 
+  return input;
+}
+
+const std::list<int> PmergeMe::initDataList(const std::vector<int>& input) {
+  std::list<int> result;
+  result.insert(result.begin(), input.begin(), input.end());
+  return result;
 }
 
 void PmergeMe::printData(const std::string prefix) {
   std::cout << prefix << ": ";
-  for (size_t i = 0; i < data.size(); ++i) {
-    std::cout << data.at(i) << " ";
-  } 
+  for (size_t i = 0; i < vec_data.size(); ++i) {
+    std::cout << vec_data.at(i) << " ";
+  }
   std::cout << std::endl;
 }
